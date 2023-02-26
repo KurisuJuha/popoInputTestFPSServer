c86d2a582130f87dc39fd56056d9d74a39725965
KYapp <kukiyomenaisapp@gmail.com>
2023-02-26 16:42:44 +0900

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using Fleck;
+using GameServer = JuhaKurisu.PopoTools.InputSystem.Server.Server;
+
+namespace popoInputTestFPS.Server;
+
+public class Program
+{
+    public static void Main(string[] args) => new Program();
+
+    private WebSocketServer webSocketServer = new WebSocketServer("ws://0.0.0.0:3000");
+
+    private Dictionary<Guid, IWebSocketConnection> connections = new();
+
+    private Program()
+    {
+        webSocketServer.Start(socket =>
+        {
+            socket.OnOpen += () =>
+            {
+                Console.WriteLine($"OnOpen: {socket.ConnectionInfo.Id}");
+                connections[socket.ConnectionInfo.Id] = socket;
+            };
+            socket.OnClose += () =>
+            {
+                Console.WriteLine($"OnClose: {socket.ConnectionInfo.Id}");
+                connections.Remove(socket.ConnectionInfo.Id);
+            };
+            socket.OnBinary += bytes =>
+            {
+                Console.WriteLine($"OnBinary: {socket.ConnectionInfo.Id}");
+                connections[socket.ConnectionInfo.Id] = socket;
+            };
+        });
+
+        while (true) ;
+    }
+
+    private void BroadCast(byte[] bytes)
+    {
+        foreach (var socket in connections.Values)
+            socket.Send(bytes);
+    }
+}
